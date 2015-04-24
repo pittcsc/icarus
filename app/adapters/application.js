61544b09bf624c293f6a8ab598aa587541b7f438
Alex LaFroscia <alex@lafroscia.com>
2015-04-24 03:58:45 -0400

    Talk to JSON API

---
+++ app/adapters/application.js
@@ -0,0 +1,8 @@
+import JsonApiAdapter from 'ember-json-api/json-api-adapter';
+import config from '../config/environment';
+
+export default JsonApiAdapter.extend({
+  host: config.apiURL,
+  namespace: 'api'
+});
+
