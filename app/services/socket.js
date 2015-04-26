53f051f1204e6aa2d33c5ae2fa6b456b910f563d
Alex LaFroscia <alex@lafroscia.com>
2015-04-26 17:29:19 -0400

    Check for feature availability

--- app/services/socket.js
+++ app/services/socket.js
@@ -7,12 +7,18 @@ export default Ember.Service.extend({
   // Store the Socket.io connection
   connection: null,
 
+  // Does the browser support websockets?
+  available: function() {
+    return "WebSocket" in window;
+  }.property(),
+
   // Store table of events and IDs
   events: {},
 
   // Called from an initializer to ensure that the socket connection exists
   // before allowing the JS app to boot
   setup: function() {
+    if (!this.get('available')) { return; }
     return new Ember.RSVP.Promise((resolve) => {
       var socket = new WebSocket(`ws://${config.apiURL}/socket/`);
 
@@ -23,34 +29,41 @@ export default Ember.Service.extend({
       socket.onmessage = (evt) => {
         const msg = new Message(evt.data);
         var cb;
+        // Make sure the callback exists
         try {
           cb = this.get('events')[msg.type][msg.id];
         } catch (e) {
           return;
         }
+        // Callback could be null, if the object was deleted
         if (cb !== null) {
           cb();
         }
       };
 
       this.set('connection', socket);
+      // TODO: resolve once the connection has been verified
+      // TODO: handle WebSocket not being available
       resolve();
 
     });
   },
 
   on: function(type, id, cb) {
+    if (!this.get('available')) { return; }
     const events = this.get('events');
     events[type] = events[type] || {};
     events[type][id] = cb;
   },
 
   off: function(type, id) {
+    if (!this.get('available')) { return; }
     const events = this.get('events');
     events[type][id] = null;
   },
 
   send: function(data) {
+    if (!this.get('available')) { return; }
     const connection = this.get('connection');
     const msg = new Message(data);
     connection.send(msg.toJSON());
