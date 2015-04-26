1248fad6236137551a54d5b08715ae62794db179
Alex LaFroscia <alex@lafroscia.com>
2015-04-26 15:24:29 -0400

    Add basic talk routing

--- app/router.js
+++ app/router.js
@@ -6,4 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.route('talks', function() {
+    this.route('talk', {path: ':id'});
+  });
+  this.route('workshops');
 });
