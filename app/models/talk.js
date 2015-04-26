32054a0d800e5dc4630e0f23a827c9c373975bca
Alex LaFroscia <alex@lafroscia.com>
2015-04-26 17:35:13 -0400

    Synchronous upvotes over socket connection

--- app/models/talk.js
+++ app/models/talk.js
@@ -3,10 +3,17 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
   title: DS.attr('string'),
+  upvotes: DS.attr('number'),
+  downvotes: DS.attr('number'),
+
+  totalvotes: function() {
+    return this.get('upvotes') - this.get('downvotes');
+  }.property('upvotes', 'downvotes'),
 
   setupSocketEvents: function() {
     this.socket.on('upvote-talk', this.get('id'), () => {
-      console.debug(this);
+      const votes = this.get('upvotes');
+      this.set('upvotes', votes + 1);
     });
   }.on('ready'),
 
