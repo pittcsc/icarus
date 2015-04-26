2126eaa424f84b77b19b542bfa11692d97c9941a
Alex LaFroscia <alex@lafroscia.com>
2015-04-26 17:23:27 -0400

    Have the talk model subscribe to the upvote event

--- app/models/talk.js
+++ app/models/talk.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string')
+
+  title: DS.attr('string'),
+
+  setupSocketEvents: function() {
+    this.socket.on('upvote-talk', this.get('id'), () => {
+      console.debug(this);
+    });
+  }.on('ready'),
+
+  teardownSocketEvents: function() {
+    this.socket.off('upcote-talk', this.get('id'));
+  }.on('didDelete')
 });
