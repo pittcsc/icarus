feaff65b4701e6877137b50e9e42d03d9378ccc0
Alex LaFroscia <alex@lafroscia.com>
2015-04-26 17:29:09 -0400

    Talk list component

--- app/components/talk-list-item.js
+++ app/components/talk-list-item.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  talk: null,
+  socket: Ember.inject.service(),
+
+  actions: {
+    upvote: function() {
+      const talk = this.get('talk');
+      this.get('socket').send({
+        id: talk.id,
+        type: 'upvote-talk'
+      });
+    }
+  }
 });
