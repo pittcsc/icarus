fb629776b45dc0b6565f3ceefe181c8bb2354ac2
Alex LaFroscia <alex@lafroscia.com>
2015-05-07 02:10:10 -0400

    Install font-awesome

--- Brocfile.js
+++ Brocfile.js
@@ -1,22 +1,21 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
 
 var app = new EmberApp({
-
+  sourcemaps: {
+    enabled: true,
+    extensions: ['js', 'scss']
+  }
 });
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+// Import FontAwesome
+app.import("vendor/font-awesome.css");
+var fontawesome = pickFiles('bower_components/fontawesome', {
+  srcDir: '/fonts',
+  files: ['**.*'],
+  destDir: 'assets/fonts',
+});
 
-module.exports = app.toTree();
+module.exports = app.toTree(fontawesome);
