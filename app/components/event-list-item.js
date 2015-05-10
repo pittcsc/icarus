017471a374c0e9a671cf9fcde4bebebd216bbfda
Alex LaFroscia <alex@lafroscia.com>
2015-05-10 19:19:45 -0400

    Update event list item

--- app/components/event-list-item.js
+++ app/components/event-list-item.js
@@ -2,31 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  event: null,
-
+  // Class name for component
   classNames: ['event-list-item'],
 
-  attributeBindings: ['lid:data-container-id'],
+  // Services
+  session: Ember.inject.service(),
 
+  // Stuff for liquid-fire
+  attributeBindings: ['lid:data-container-id'],
   lid: Ember.computed.alias('event.id'),
 
+  // Event for item, plus properties for event attributes
+  event: null,
   title: Ember.computed.alias('event.title'),
   score: Ember.computed.alias('event.totalvotes'),
 
-  socket: Ember.inject.service(),
-
   actions: {
-    /**
-     * Cast a positive vote for an event
-     */
+
+    // Cast a positive vote for an event
     upvote() {
       const event = this.get('event');
       event.upvote();
     },
 
-    /**
-     * Cast a negative vote for an event
-     */
+    // Cast a negative vote for an event
     downvote() {
       const event = this.get('event');
       event.downvote();
