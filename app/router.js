73174aa7c1c5290ade7704b45b9f42f40384f054
Alex LaFroscia <alex@lafroscia.com>
2015-07-08 23:00:19 -0400

    Ember CLI v0.2.4

--- app/router.js
+++ app/router.js
@@ -5,7 +5,9 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-export default Router.map(function() {
+Router.map(function() {
   this.route('talk', {path: 'talks/:id'});
   this.route('talks');
 });
+
+export default Router;
